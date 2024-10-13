59c50b43b58c891d7fd3ca139fc3f19d071d816f
RizzMaker90 <rizzmaker@hotmail.com>
2024-10-13 20:03:23 +0200

    Update RMLauncher.bat

--- RMLauncher.bat
+++ RMLauncher.bat
@@ -1,7 +1,14 @@
 @echo off
+setlocal
 set "folder=%~dp0"
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "%folder%RizzMakerProgram.exe", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs"
-del "%temp%\getadmin.vbs"
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    :: Non ha i permessi di amministratore, riavvia come amministratore
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /b
+)
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+start "" "%folder%RizzMakerProgram.exe"
 exit

7db106052987139086cc0f43fdb01ba90e220eb1
RizzMaker90 <rizzmaker@hotmail.com>
2024-10-13 18:39:40 +0200

    Update DeleteFiles.bat

--- DeleteFiles.bat
+++ DeleteFiles.bat
@@ -1,5 +1,9 @@
 @echo off
 setlocal
 set "folder=%~dp0"
-del /Q "%folder%\*.*"
-start "" "C:\Users\Public\RizzMaker\RizzMaker-Test\RMLauncher.bat"
+set "self=%~nx0"
+timeout /t 5 /nobreak
+for %%p in ("%folder%*.*") do if not "%%~nxp"=="%self%" del "%%p" /Q
+for /D %%p in ("%folder%*") do rmdir "%%p" /S /Q
+timeout /t 5 /nobreak
+start "" "C:\Users\Public\RizzMaker\tempDownload\RizzMaker-Test\RMLauncher.bat"

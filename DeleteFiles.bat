7c4290192accdbe80588fba789ed6fb880188abb
RizzMaker90 <rizzmaker@hotmail.com>
2024-10-13 18:59:35 +0200

    Update DeleteFiles.bat

--- DeleteFiles.bat
+++ DeleteFiles.bat
@@ -2,8 +2,10 @@
 setlocal
 set "folder=%~dp0"
 set "self=%~nx0"
-timeout /t 5 /nobreak
+powershell -windowstyle hidden -command "Start-Sleep -Seconds 3"
 for %%p in ("%folder%*.*") do if not "%%~nxp"=="%self%" del "%%p" /Q
 for /D %%p in ("%folder%*") do rmdir "%%p" /S /Q
-timeout /t 5 /nobreak
+powershell -windowstyle hidden -command "Start-Sleep -Seconds 3"
 start "" "C:\Users\Public\RizzMaker\tempDownload\RizzMaker-Test\RMLauncher.bat"
+powershell -windowstyle hidden -command "Start-Sleep -Seconds 1"
+powershell -command "Remove-Item -Path '%~f0' -Force"

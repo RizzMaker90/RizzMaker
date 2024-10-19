820ef6f0041cb098830e066093e4fee994bd9f44
RizzMaker90 <rizzmaker@hotmail.com>
2024-10-19 14:21:43 +0200

    Delete DeleteFiles.bat

--- DeleteFiles.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "folder=%~dp0"
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "%folder%RizzMakerProgram.exe", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs"
-del "%temp%\getadmin.vbs"
-exit
